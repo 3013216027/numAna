2abd9e13aca4baf19acba61b1294937b23edbdbf
zhengdongjian <zhengdongjian@tju.edu.cn>
2016-04-27 14:04:06 +0800

    debug msg format correct & finish practice 4.8

--- integral/4_8.cpp
+++ integral/4_8.cpp
@@ -1,40 +1,19 @@
 /* **********************************************
 
-  File Name: romberg.cpp
+  File Name: 4_8.cpp
 
   Author: zhengdongjian@tju.edu.cn
 
-  Created Time: Wed Apr 27 11:55:03 2016
+  Created Time: Wed Apr 27 12:59:11 2016
 
 *********************************************** */
 #include <bits/stdc++.h>
 using namespace std;
 
-const bool DEBUG = false;
+const double PI = acos(-1);
 
-/**
- * 区间[a,b]求func(x)的积分
- * 龙贝格求积基于梯形公式和外推加速，算法如下:
- * (1) 取k=1,h=b-a,求T_0^{(0)}=h/2 * [f(a) + f(b)].
- *   for 1->k
- * (2) 求T_0((b-a) / (2^h)), 按公式T_{2n}=h/4*\sum_{k=0}^{n-1}*[f(x_k)+f(x_{k+1})] + h/2*\sum_{k=0}^{n-1}*f(x_{k+1/2})算
- * (3) 求加速值，T_m^{(k)}=4^m/(4^m-1)*T_{m-1}^{(k+1)}-1/(4^m-1)*T_{m-1}^{(k)}
- * (4) 若|T_k{(0)}-T_{k-1}^{(0)}|<ε，结束，取T_k{(0)}≈I，否则令k=k+1，转2
- * 上述求解过程就是：
- * k      h     T_0{k}    T_1{k}   T_2{k} ...
- * 0     b-a    T_0{0}
- * 1   (b-a)/2  T_0{1}    T_1{0}
- * 2   (b-a)/4  T_0{2}    T_1{1}    T_2{0}
- * 3   (b-a)/8  ...
- *
- * *      *       1
- *                |
- * *      *       2   ->   3
- *                |
- * *      *       4   ->   5   ->   6
- *                |
- * *      *       7   ->   8 ...
- */
+const bool DEBUG = true;
+
 double romberg(double a, double b, double (*func)(double), double EPS = 1e-6) {
     int k = 1;
     double h = b - a, h2, h4;
@@ -51,6 +30,7 @@ double romberg(double a, double b, double (*func)(double), double EPS = 1e-6) {
         //row k
         h2 = 0.5 * h; // (h/2)
         h4 = 0.25 * h; // (h/4)
+		bool stop = false;
         for (int i = k, m = 0; i >= 0; i--, m++) {
             //calculate T_{m}^{i}
             //T[k][m]=T_{m}{0}=h/4*\sum_{k=0}^{n-1}*[f(x_k)+f(x_{k+1})] + h/2*\sum_{k=0}^{n-1}*f(x_{k+1/2})
@@ -69,23 +49,41 @@ double romberg(double a, double b, double (*func)(double), double EPS = 1e-6) {
 			if (DEBUG) {
 				fprintf(stdout, "T[%d][%d]=%.6f ", k, m, T[k][m]);
 			}
+			if (i != 0 && fabs(T[k][m] - T[k - 1][m]) < EPS) {
+				stop = true;
+			}
         }
 		if (DEBUG) {
 			fprintf(stdout, "\n");
 		}
         h = h2; //reduce delta
-        if (fabs(T[k][0] - T[k - 1][0]) < EPS) {
-            break;
-        }
+		if (stop) {
+			break;
+		}
     }
-    return T[k][0];
+    return T[k][k];
 }
 
-double fx(double x) {
-    return pow(x, 1.5);
+double f1(double x) {
+	return exp(-x);
+}
+
+double f2(double x) {
+	return x * sin(x);
+}
+
+double f3(double x) {
+	return x * sqrt(1 + x * x);
 }
 
 int main() {
-    fprintf(stdout, "∫(0->1){x^1.5} = %.6f\n", romberg(0, 1, fx));
+	double eps = 1e-5;
+	fprintf(stdout, "第四章第8题：\n");
+	fprintf(stdout, "(1) 2/sqrt(π)*∫(0->1){e^-x}dx = %.6f\n", 2.0 / sqrt(PI) * romberg(0, 1, f1, eps) + 1e-6);
+	fprintf(stdout, "--------------------------------------------------------------------------------------\n");
+	fprintf(stdout, "(2) ∫(0->2π){x*sin(x)}dx = %.6f\n", romberg(0, 2 * PI, f2, eps) + 1e-6);
+	fprintf(stdout, "--------------------------------------------------------------------------------------\n");
+	fprintf(stdout, "(3) ∫(0->3){x*sqrt(1+x^2)}dx = %.6f\n", romberg(0, 3, f3, eps) + 1e-6);
+	fprintf(stdout, "--------------------------------------------------------------------------------------\n");
     return 0;
 }
